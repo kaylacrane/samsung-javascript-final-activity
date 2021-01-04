592b4a7c94762732ef26f2910042c9866c32f6af
Kayla Jean Crane <kayla.crane@gmail.com>
2021-01-04 12:19:39 +0100

    update css and add regex validation

--- main.js
+++ main.js
@@ -5,26 +5,36 @@ const accountButton = document.getElementById("show-account");
 const dayButton = document.getElementById("show-day");
 
 const ratingInput = document.getElementById("rating-input");
-const accountInputs = document.getElementById("bank-details").querySelectorAll("input");
+const accountInputs = document
+  .getElementById("bank-details")
+  .querySelectorAll("input");
 const dateInput = document.getElementById("date-input");
 
 // event handlers
 function showRating() {
-    alert(`Has valorado con ${ratingInput.value} puntos`);
+  alert(`Has valorado con ${ratingInput.value} puntos`);
 }
 
 function showAccount() {
-    let accountNumber = [];
-    accountInputs.forEach(node => {
-        accountNumber.push(node.value);
-    })
-    alert(`Le informamos que su cuenta bancaria es: ${accountNumber.join("-")}`)
+  let accountNumber = [];
+  accountInputs.forEach((node) => {
+    accountNumber.push(node.value);
+  });
+  alert(`Le informamos que su cuenta bancaria es: ${accountNumber.join("-")} `);
 }
 
 function showDay() {
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const day = daysOfWeek[new Date(dateInput.value).getUTCDay()];
-    alert(`La fecha seleccionada en el elemento de fecha es un ${day}`)
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = daysOfWeek[new Date(dateInput.value).getUTCDay()];
+  alert(`La fecha seleccionada en el elemento de fecha es un ${day}`);
 }
 
 // event listeners
